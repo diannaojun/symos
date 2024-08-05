10d00470dbd527d7897a3043f8ccafed9eb13a54
DiannaoJun <aheiwuchang@163.com>
2024-08-05 14:56:06 +0000

    1
    Signed-off-by: DiannaoJun <aheiwuchang@163.com>

---
+++ kernel/lib/lloader/main.h
@@ -0,0 +1,12 @@
+/* Symbol DT
+ * SymOS
+ * DiannaoJun
+ * 2024-08-05
+ */
+#ifndef _SYM_KERNEL_MAIN_H_
+#define _SYM_KERNEL_MAIN_H_
+
+#include <IA64/stddef.h>
+#include <lib/lloader/asm.h>
+
+#endif //_SYM_KERNEL_MAIN_H_
