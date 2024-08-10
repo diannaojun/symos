ca6b4db2b8a412e171519c69a1fd68a813536f6c
diannaojun <aheiwuchang@163.com>
2024-08-10 09:13:41 +0800

    Update lloader.c

--- kernel/lloader.c
+++ kernel/lloader.c
@@ -5,16 +5,23 @@
  */
 
 #include <lib/lloader/main.h>
+
+void show(u64 x, void* s){
+    static char _t[]="0123456789ABCDEF",i;
+    for(i=0;i<16;++i,x>>=4){
+        *((char*)(s+(i<<1)))=_t[x&15];
+        *((char*)(s+(i<<1)+1))=0x0f;
+    }
+}
+
 int main (void){
     size_t itr;
     void *secbuf0 = (void*)0x0202000ull;
     void *secbuf1 = (void*)0x0202200ull;
     void *secbuf2 = (void*)0x0202400ull;
     ins_init();
-    *((char*)0xb8000)='0'+((u64)(((struct INS_CONSOLE_OPT*)(ins[2].ins_operation))->clear))&7;
-    *((char*)0xb8001)=0x0f;
-    *((char*)0xb8002)='0'+((u64)(*ins_scrn_txt_clr))&7;
-    *((char*)0xb8003)=0x0f;
+    show((u64)(((struct INS_CONSOLE_OPT*)(ins[2].ins_operation))->clear),0xb8000);
+    show((u64)(((struct INS_CONSOLE_OPT*)(ins[2].ins_operation))->clear),0xb8040);
     // (*(((struct INS_CONSOLE_OPT*)(ins[2].ins_operation))->clear))(ins[2].ins_data);
     // (((struct INS_DISK_OPT*)(ins[0].ins_operation))->read)(ins[0].ins_data, secbuf0, 0, 1);
     // SMYFS_INS_LOAD(&ins[0], secbuf0, ((struct SMYFS*)secbuf0)->root_start, secbuf2);
