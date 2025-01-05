f27b0e87e0ed1ba761ffb1924f5b2d72f0f87e8e
diannaojun <aheiwuchang@163.com>
2025-01-05 21:58:01 +0800

    update

--- arch/amd64/mbr.asm
+++ arch/amd64/mbr.asm
@@ -1,9 +1,6 @@
 %include "config.inc"
-
 org ADDR_SEG_MBR_PROG << 4
 bits 16
-
-%include "config.inc"
 
 jmp _start
 
@@ -11,151 +8,119 @@ ident:
     .flag       db "symbol-os", 0
     .version    dw 0x0300
     .magic      db 0x55, 0xaa
-
-align 16
+packet:
+    .size       dw 0x0010
+    .count      dw 0x0001
+    .offset     dw 0x7e00
+    .segment    dw 0x0000
+    .address    dq 0x0000000000000001
+    .ptr        dq 0x0000000000000000
+messages:
+    .err_head       db "[L16::err(", 0
+    .err_foot       db ")] ", 0
 _start:
-    mov ax, cs
+    xor ax, ax
     mov ss, ax
     mov es, ax
     mov ds, ax
-    xor sp, sp
-    xor bp, sp
-    xor si, si
-    xor di, di
+    mov ax, ADDR_SEG_MBR_PROG << 4
+    mov sp, ax
+    mov bp, ax
+    mov si, ax
+    mov di, ax
     mov [ident.magic], dl
-; log_out:
-;     call lost_screen
-;     call clean_screen
-; read_mem:
-;     mov ah, 0x88
-;     int 0x15
-;     mov [info.memory], ax
-; read_video:
-;     mov ah, 0x0f
-;     int 0x10
-;     mov [info.v_page], bh
-;     mov [info.v_mode], al
-;     mov [info.v_width], ah
-;     mov ah, 0x12
-;     mov bl, 0x10
-;     int 0x10
-;     mov [info.v_data0], ax
-;     mov [info.v_data1], bx
-;     mov [info.v_data2], cx
-read_setup:
-    mov word [packet.offset], 0
-    mov word [packet.segment], ADDR_SEG_SET_PROG
-    mov word [packet.blocks], DATA_LEN_OF_SETUP
-    mov byte [packet.address], 0x40
+check_int13hx:
+    mov ah, 0x41
+    mov bx, 0x55aa
+    int 0x13
+    mov ax, 0x0001
+    jc errors
+    cmp bx, 0xaa55
+    jne errors
+read_sdmpart:
+    mov ah, 0x42
     mov dl, [ident.magic]
     mov si, packet
-    call read_lba
-    ; mov si, messages.log_head
-    ; call puts
-    ; mov si, ident
-    ; call puts
-    ; mov al, ' '
-    ; call putc
-    ; mov ax, [ident.version]
-    ; call putx
-    ; mov si, messages.endline
-    ; call puts
-    jmp ADDR_SEG_SET_PROG : 0
-; puts:               ; void puts (si)
-;     .puts_loop:
-;         mov al, [si]
-;         inc si
-;         test al, al
-;         jz .puts_ret
-;         call putc
-;         jmp .puts_loop
-;     .puts_ret:
-;         ret
-; putx:               ; void putx (ax)
-;     mov cx, 4
-;     .putx_loop:
-;         push ax
-;         shr ax, 12
-;         mov si, ax
-;         cmp al, 0x0a
-;         jge .big10
-;             add al, '0'
-;             jmp .show
-;         .big10:
-;             add al, 'a'-10
-;         .show:
-;         push cx
-;         call putc
-;         pop cx
-;         pop ax
-;         shl ax, 4
-;     loop .putx_loop
-;     ret
-; putc:               ; void putc (al)
-;     mov ah, 0x0e
-;     mov bx, 0x0f
-;     int 0x10
-;     ret
-read_lba:
-    mov ah, 0x42
     int 0x13
-    jc error
-    ret
-; clean_screen:
-;     mov ah, 0x06
-;     xor cx, cx
-;     mov al, cl
-;     mov dx, 0xffff
-;     mov bh, 0x07
-;     int 0x10
-;     ret
-; lost_screen:
-;     mov ah, 0x02
-;     xor bx, bx
-;     xor dx, dx
-;     int 0x10
-;     ret
-error:
-    ; mov si, messages.err_head
-    ; call puts
-    ; mov ax, 0x0001
-    ; call putx
-    ; mov si, messages.err_foot
-    ; call puts
-    ; mov si, messages.io_error
-    ; call puts
-    ; mov si, messages.endline
-    ; call puts
+    mov ax, 0x0002
+    jc errors
+    mov cx, 0x04
+    mov si, 0x7e10
+    mov di, packet.address
+    .loop:
+        mov ax, [si]
+        mov [di], ax
+        add si, 0x02
+        add di, 0x02
+        loop .loop
+    mov ax, [si]
+    mov [packet.count], ax
+read_bootsect:
+    mov word [packet.offset], 0x0000
+    mov word [packet.segment], ADDR_SEG_SET_PROG
+    mov ah, 0x42
+    mov dl, [ident.magic]
+    mov si, packet
+    int 0x13
+    mov ax, 0x0003
+    jc errors
+    jmp ADDR_SEG_SET_PROG:0
+errors:
+    push ax
+    mov ah, 0x02
+    xor bx, bx
+    xor dx, dx
+    int 0x10
+    mov ah, 0x06
+    xor cx, cx
+    mov al, cl
+    mov dx, 0xffff
+    mov bh, 0x07
+    int 0x10
+    mov si, messages.err_head
+    call puts
+    pop ax
+    push ax
+    call putx
+    mov si, messages.err_foot
+    call puts
 dead:
     jmp $
-packet:
-    dw 0x10
-    .blocks     dw 0
-    .offset     dw 0
-    .segment    dw 0
-    .address    dq 0
-    .bufferl    dq 0
-messages:
-    .endline    db 10, 13, 0
-    .log_head   db "[L16::log] ", 0
-    .err_head   db "[L16::err(", 0
-    .err_foot   db ")] ", 0
-    .io_error   db "I/O Error", 0
-
-times 0x150-($-$$) db 0x00
-
-gdt_info:
-    dw gdt.end - gdt
-    dd gdt
-idt_info:
-    times 3 dw 0
-
-gdt:
-    .null   gdt_null
-    .code   gdt_segment 0x00000000, 0xffffffff, sta_prog_x | sta_prog_xr, stt_32def | stt_limitalign4kb
-    .data   gdt_segment 0x00000000, 0xffffffff, sta_prog_d | sta_prog_wr, stt_32def | stt_limitalign4kb
-    .stack  gdt_segment 0x00008000, 0x08000 - 0x00500, sta_prog_d | sta_prog_wr | sta_prog_gd, stt_32def
-    .end:
+puts:               ; void puts (si)
+    .puts_loop:
+        mov al, [si]
+        inc si
+        test al, al
+        jz .puts_ret
+        call putc
+        jmp .puts_loop
+    .puts_ret:
+        ret
+putx:               ; void putx (ax)
+    mov cx, 4
+    .putx_loop:
+        push ax
+        shr ax, 12
+        mov si, ax
+        cmp al, 0x0a
+        jge .big10
+            add al, '0'
+            jmp .show
+        .big10:
+            add al, 'a'-10
+        .show:
+        push cx
+        call putc
+        pop cx
+        pop ax
+        shl ax, 4
+    loop .putx_loop
+    ret
+putc:               ; void putc (al)
+    mov ah, 0x0e
+    mov bx, 0x0f
+    int 0x10
+    ret
 
 times (510-64)-($-$$) db 0x00
 
--- arch/amd64/setup.asm
+++ arch/amd64/setup.asm
@@ -1,33 +1,38 @@
 %include "config.inc"
-
 org ADDR_SEG_SET_PROG << 4
 bits 16
-
-jmp _start
 
 ; ------ 00500
 ; STACK
 ; ------ 08000
 
+jmp _start
 
+gdt:
+    .null   gdt_null
+    .code   gdt_segment 0x00000000, 0xffffffff, sta_prog_x | sta_prog_xr, stt_32def | stt_limitalign4kb
+    .data   gdt_segment 0x00000000, 0xffffffff, sta_prog_d | sta_prog_wr, stt_32def | stt_limitalign4kb
+    .stack  gdt_segment 0x00008000, 0x08000 - 0x00500, sta_prog_d | sta_prog_wr | sta_prog_gd, stt_32def
+    .end:
+gdt_info:
+    dw gdt.end - gdt
+    dd gdt
+idt_info:
+    times 3 dw 0
 
-
-align 16
 _start:
 mode16:
-    cli
-
+    mov ax, cs
+    mov ds, ax
+    lgdt [ds:gdt_info]
+    lidt [ds:gdt_info]
     in al, 0x92
     or al, 0x02
     out 0x92, al
-
-    lgdt [0x7d50]
-    lidt [0x7d56]
-
+    cli
     mov eax, cr0
     or eax, 0x00000001
     mov cr0, eax
-
     jmp dword 0x0008:mode32
 
 bits 32
