550f1bb8ef0abb054fbb156136aeb9f3e7e3fe45
CLydq <clydq@outlook.com>
2025-01-22 13:32:00 +0800

    merge branch develop

--- arch/amd64/kernel.c
+++ arch/amd64/kernel.c
@@ -1,11 +1,41 @@
 #include <kernel/asm.h>
-#include <kernel/init.h>
-#include <kernel/struct.h>
 
-int main (void) {
-    init_gdt();
-    init_idt();
+void putck(char ch);
+void putsk(const char *str);
+int main(void);
+
+int main(void) {
+    putsk((char *)0x980c7);
     breakpoint();
-    for(; ; );
+    putsk("FROM C: Loading Kernel64...\n\r");
+    breakpoint();
+    for (;;)
+        ;
     return 0;
+}
+
+void putck(char ch) {
+    ch = toascii(ch);
+    switch (ch) {
+    case '\t':
+        *((short *)0x7e02) = (*((short *)0x7e02) + 7) & 0xfff8;
+        break;
+    case '\n':
+        *((short *)0x7e02) += 160;
+        break;
+    case '\r':
+        *((short *)0x7e02) = (*((short *)0x7e02) + 7) & 0xfff8;
+        break;
+    default:
+        *(short *)(size_t)(0xb8000 | *((short *)0x7e02)) = ch | 0x0f00;
+        *((short *)0x7e02) += 2;
+    }
+    return;
+}
+
+void putsk(const char *str) {
+    static size_t i;
+    for (i = 0; str[i]; ++i)
+        putck(str[i]);
+    return;
 }
